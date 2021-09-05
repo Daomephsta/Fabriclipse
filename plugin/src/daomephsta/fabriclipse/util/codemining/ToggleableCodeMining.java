608a570a7e2616a9da23c99cde5cb8158fab9f1f
Daomephsta <dtnilfisk@gmail.com>
2021-09-06 11:09:00 +1200

    Rework LineHeaderCodeMining to account for special casing of LineHeaderCodeMining

--- plugin/src/daomephsta/fabriclipse/util/codemining/ToggleableCodeMining.java
+++ plugin/src/daomephsta/fabriclipse/util/codemining/ToggleableCodeMining.java
@@ -4,92 +4,143 @@ import java.util.function.Consumer;
 
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences.IPreferenceChangeListener;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences.PreferenceChangeEvent;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.Position;
 import org.eclipse.jface.text.codemining.AbstractCodeMining;
 import org.eclipse.jface.text.codemining.ICodeMiningProvider;
-import org.eclipse.jface.text.source.inlined.Positions;
+import org.eclipse.jface.text.codemining.LineContentCodeMining;
+import org.eclipse.jface.text.codemining.LineHeaderCodeMining;
 import org.eclipse.swt.events.MouseEvent;
 
 // Minings are toggled instead of the provider returning an empty list. The latter is ideal,
 // but it's unclear how to get Eclipse to recompute and draw minings when the toggle changes.
-public class ToggleableCodeMining extends AbstractCodeMining
+public abstract class ToggleableCodeMining
 {
-    private final int line, column;
-    private final String prefQualifier;
-    private final IPreferenceChangeListener prefChangeListener;
-    private boolean active;
-
-    private ToggleableCodeMining(Position position, ICodeMiningProvider provider,
-        Consumer<MouseEvent> action, int line, int column, String fullPrefKey)
+    private static class Toggle implements IPreferenceChangeListener
     {
-        super(position, provider, action);
-        this.line = line;
-        this.column = column;
-        int prefSeparator = fullPrefKey.lastIndexOf('.');
-        this.prefQualifier = fullPrefKey.substring(0, prefSeparator);
-        String prefKey = fullPrefKey.substring(prefSeparator + 1);
-        IEclipsePreferences prefs = InstanceScope.INSTANCE.getNode(prefQualifier);
-        this.active = prefs.getBoolean(prefKey, false);
-        this.prefChangeListener = e ->
+        private final String prefQualifier, prefKey;
+        private boolean active;
+
+        Toggle(String fullPrefKey)
+        {
+            int prefSeparator = fullPrefKey.lastIndexOf('.');
+            this.prefQualifier = fullPrefKey.substring(0, prefSeparator);
+            this.prefKey = fullPrefKey.substring(prefSeparator + 1);
+            IEclipsePreferences prefs = InstanceScope.INSTANCE.getNode(prefQualifier);
+            this.active = prefs.getBoolean(prefKey, false);
+            prefs.addPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void preferenceChange(PreferenceChangeEvent e)
         {
             if (e.getKey().equals(prefKey))
                 this.active = Boolean.valueOf((String) e.getNewValue());
-        };
-        prefs.addPreferenceChangeListener(prefChangeListener);
+        }
+
+        public void dispose()
+        {
+            InstanceScope.INSTANCE.getNode(prefQualifier).removePreferenceChangeListener(this);
+        }
     }
 
-    public static ToggleableCodeMining inline(Position position, IDocument document,
+    public static AbstractCodeMining inline(Position position, IDocument document,
         ICodeMiningProvider provider, Consumer<MouseEvent> clickAction, String prefKey)
         throws BadLocationException
     {
         int line = document.getLineOfOffset(position.getOffset());
         int column = position.getOffset() - document.getLineOffset(line);
-        return new ToggleableCodeMining(position, provider, clickAction,
-            line, column, prefKey);
+        return new Inline(position, provider, clickAction,
+            line, column, new Toggle(prefKey));
     }
 
-    public static ToggleableCodeMining header(int beforeLineNumber, IDocument document,
+    public static AbstractCodeMining header(int line, IDocument document,
         ICodeMiningProvider provider, Consumer<MouseEvent> clickAction, String prefKey)
         throws BadLocationException
     {
-        Position position = Positions.of(beforeLineNumber, document, true);
-        return new ToggleableCodeMining(position, provider, clickAction,
-            beforeLineNumber - 1, 0, prefKey);
+        return new Header(line, document, provider, clickAction, new Toggle(prefKey));
     }
 
-    public boolean isActive()
+    private static class Inline extends LineContentCodeMining
     {
-        return active;
-    }
+        private final int line, column;
+        private final Toggle toggle;
 
-    @Override
-    public String getLabel()
-    {
-        return isActive() ? super.getLabel() : null;
-    }
+        private Inline(Position position, ICodeMiningProvider provider,
+            Consumer<MouseEvent> clickAction, int line, int column, Toggle toggle)
+        {
+            super(position, provider, clickAction);
+            this.line = line;
+            this.column = column;
+            this.toggle = toggle;
+        }
 
-    @Override
-    public Consumer<MouseEvent> getAction()
-    {
-        return isActive() ? super.getAction() : null;
-    }
+        @Override
+        public String getLabel()
+        {
+            return toggle.active ? super.getLabel() : null;
+        }
 
-    @Override
-    public void dispose()
-    {
-        InstanceScope.INSTANCE.getNode(prefQualifier).removePreferenceChangeListener(prefChangeListener);
-        super.dispose();
-    }
+        @Override
+        public Consumer<MouseEvent> getAction()
+        {
+            return toggle.active ? super.getAction() : null;
+        }
 
-    @Override
-    public String toString()
-    {
-        if (column == 0)
-            return String.format("L%d: %s", line, super.getLabel());
-        else
+        @Override
+        public void dispose()
+        {
+            super.dispose();
+            toggle.dispose();
+        }
+
+        @Override
+        public String toString()
+        {
             return String.format("L%d @ %d: %s", line, column, super.getLabel());
+        }
+    }
+
+    // Must extend LineHeaderCodeMining, as it is special cased
+    private static class Header extends LineHeaderCodeMining
+    {
+        private final int line;
+        private final Toggle toggle;
+
+        private Header(int line, IDocument document, ICodeMiningProvider provider,
+            Consumer<MouseEvent> action, Toggle toggle) throws BadLocationException
+        {
+            super(line, document, provider, action);
+            this.line = line;
+            this.toggle = toggle;
+        }
+
+        @Override
+        public String getLabel()
+        {
+            return toggle.active ? super.getLabel() : null;
+        }
+
+        @Override
+        public Consumer<MouseEvent> getAction()
+        {
+            return toggle.active ? super.getAction() : null;
+        }
+
+        @Override
+        public void dispose()
+        {
+            super.dispose();
+            toggle.dispose();
+        }
+
+        @Override
+        public String toString()
+        {
+            return String.format("L%d: %s", line, super.getLabel());
+        }
     }
 }
